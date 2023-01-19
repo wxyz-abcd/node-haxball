c081155962bf3b16027b2eebedfdf5abeb659a07
abc <mail.api.test.1.1.1@gmail.com>
2023-01-19 16:16:17 +0300

    browser multijoin example added

--- examples_web/src/multiJoinRoom.js
+++ examples_web/src/multiJoinRoom.js
@@ -23,14 +23,8 @@ const API = abcHaxballAPI({
 
 const { OperationType, VariableType, ConnectionState, AllowFlags, Callback, Utils, Room, Replay, RoomConfig, Plugin, Renderer, Impl } = API;
 
-Callback.add("KeyDown"); // this defines room._onKeyDown(). We will use this callback when keyDown event happens. It will trigger all roomConfig, plugin and renderer callbacks.
-Callback.add("KeyUp"); // this defines room._onKeyUp(). We will use this callback when keyUp event happens. It will trigger all roomConfig, plugin and renderer callbacks.
-Callback.add("MouseDown"); // this defines room._onMouseDown(). We will use this callback when mouseDown event happens. It will trigger all roomConfig, plugin and renderer callbacks.
-Callback.add("MouseUp"); // this defines room._onMouseUp(). We will use this callback when mouseUp event happens. It will trigger all roomConfig, plugin and renderer callbacks.
-
 var r_id_elem = document.getElementById("r_id");
 var p_flag_elem = document.getElementById("p_flag");
-var notHeadless = document.getElementById("notHeadless");
 
 function getQueriesAsJSON() {
   return window.location.search.substring(1).split("&").reduce((acc, part)=>{
@@ -49,33 +43,25 @@ if (q.id!=null)
 var joinRoom = function(){
   var r_id = r_id_elem.value;
   var r_pass = document.getElementById("r_pass").value;
-  var p_ak = document.getElementById("p_ak").value;
+  var jc = document.getElementById("jc").value;
   var p_name = document.getElementById("p_name").value;
   var p_avatar = document.getElementById("p_avatar").value;
   var p_lat = document.getElementById("p_lat").value;
   var p_lon = document.getElementById("p_lon").value;
   var p_flag = p_flag_elem.value;
-  var token = document.getElementById("token").value;
-  var alerted = false;
-  var authPromise = null;
 
   if (r_id.length<8 || r_id.length>15){
     alert("Room id length is not ideal.");
     return;
   }
-  if (p_ak=="")
-    authPromise = Utils.generateAuth();
-  else{
-    if (p_ak.length<120 || p_ak.length>150){
-      alert("Player auth key length is not ideal.");
-      return;
-    }
-    authPromise = Utils.authFromKey(p_ak);
-  }
   if (r_pass=="")
     r_pass = null;
-  if (token=="")
-    token = null;
+
+  jc = parseInt(jc);
+  if (isNaN(jc) || !isFinite(jc) || jc<1 || jc>1000){
+    alert("Join count should be an integer between 1 and 1000 inclusive.");
+    return;
+  }
 
   // these checks are necessary when you are client.
   if (p_name.length>25){
@@ -101,54 +87,18 @@ var joinRoom = function(){
     alert("Player's longitude must be a real number between -180 and 180 inclusive.");
     return;
   }
-  authPromise.then((x)=>{
-    var authObj;
-    if (p_ak=="")
-      [p_ak, authObj] = x;
-    else
-      authObj = x;
-    Room.join({
-      id: r_id, 
-      password: r_pass, 
-      token: token,
-      authObj: authObj
-    }, {
-      storage: {
-        crappy_router: false,
-        player_name: p_name,
-        player_auth_key: p_ak,
-        avatar: p_avatar,
-        fps_limit: 0,
-        geo: {
-          lat: p_lat,
-          lon: p_lon,
-          flag: p_flag
-        },
-        extrapolation: 0
-      }, 
-      renderer: null, // You may pass the renderer directly here. I have headless mode on the same page here, so I have to set the renderer later for the page to be more lightweight.
-      plugins: [new plugins.autoPlay_defensive(API)],
-      onSuccess: roomCallback,
-      onRequestRecaptcha: ()=>{
-        alert("Token rejected. Get a fresh token first!");
-      },
-      onFailure: (x)=>{
-        if (alerted)
-          return;
-        alerted = true;
-        alert(x);
-      },
-      onLeave: (x)=>{
-        if (alerted)
-          return;
-        alerted = true;
-        alert(x);
-      }
-    });
-  }).catch((ex)=>{
-    console.log(ex);
-    alert("Auth key error.");
-  });
+
+  function replaceFunctions(str, iStr){
+    return str.replaceAll(/\$i/g, iStr).replaceAll(/\$r([0-9]+)/g, (matched, group, index)=>(""+(1+Math.floor(Math.random()*parseInt(group)))));
+  }
+
+  for (var i=1;i<=jc;i++){
+    var iStr = ""+i;
+    var rPass = r_pass && replaceFunctions(r_pass, iStr);
+    var pName = replaceFunctions(p_name, iStr);
+    var pAvatar = replaceFunctions(p_avatar, iStr);
+    new Bot(r_id, rPass, pName, pAvatar, p_lat, p_lon, p_flag);
+  }
 };
 
 fetch("./countries.json").then(response => {
@@ -159,24 +109,56 @@ fetch("./countries.json").then(response => {
 
 importPlugins(["autoPlay_defensive"], ()=>{
   console.log("plugins are ready", plugins);
-  importRenderers(["defaultRenderer_minified"], ()=>{
-    console.log("renderers are ready", renderers);
-  });
 });
 
-var roomCallback = function(room){
-  if (notHeadless.checked){ // if we want a canvas
-    gameWindow = window.open("./game.html","Custom Haxball Client","width=800,height=600");
-    gameWindow.onload = ()=>{
-      //window.close(); // because we don't need this window any more.
-      return [API, room, plugins, renderers];
-    }
-    return;
-  }
-  headlessRoomCallback(room);
-};
+function Bot(id, pass, name, avatar, lat, lon, flag){
+  Utils.generateAuth().then(([playerAuthKey, authObj])=>{
+    var alerted = false;
+    Room.join({
+      id: id, 
+      password: pass, 
+      token: null,
+      authObj: authObj
+    }, {
+      storage: {
+        crappy_router: false,
+        player_name: name,
+        player_auth_key: playerAuthKey,
+        avatar: avatar,
+        fps_limit: 0,
+        geo: {
+          lat: lat,
+          lon: lon,
+          flag: flag
+        },
+        extrapolation: 0
+      }, 
+      renderer: null, // Don't render anything while we are trying to join multiple rooms simultaneously. :)
+      plugins: [new plugins.autoPlay_defensive(API)],
+      onSuccess: roomCallback,
+      onRequestRecaptcha: ()=>{
+        console.log("This is a recaptcha protected room. You can't use this tool with it!");
+      },
+      onFailure: (x)=>{
+        if (alerted)
+          return;
+        alerted = true;
+        console.log(x);
+      },
+      onLeave: (x)=>{
+        if (alerted)
+          return;
+        alerted = true;
+        console.log(x);
+      }
+    });
+  }).catch((ex)=>{
+    console.log(ex);
+  });
 
-var headlessRoomCallback = function(room){
-  // This is a headless bot test:
-  room.sendChat("Hello, everybody!");
-};
+  var roomCallback = function(room){
+    // This is a headless bot test:
+    room.sendChat("Hello, everybody! My name is "+name+",");
+    room.sendChat("an auto-playing bot programmed by abc.");
+  };
+}
